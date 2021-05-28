749d9e60961bd20d3f1414f31c179fabbd5b1175
satyanarayana reddy <36239336+saisatyasarath@users.noreply.github.com>
2021-05-28 10:43:52 +0530

    Add files via upload

--- vitmedia.sql
+++ vitmedia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 29, 2021 at 09:04 AM
+-- Generation Time: May 28, 2021 at 07:08 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.3
 
@@ -29,38 +29,104 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `comments` (
-  `comment_id` int(100) NOT NULL,
-  `post_id` varchar(100) NOT NULL,
-  `user_id` varchar(100) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `content_comment` varchar(100) NOT NULL,
-  `image` varchar(100) NOT NULL,
-  `created` varchar(100) NOT NULL
+  `commentid` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `pid` int(11) NOT NULL,
+  `description` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `comments`
 --
 
-INSERT INTO `comments` (`comment_id`, `post_id`, `user_id`, `name`, `content_comment`, `image`, `created`) VALUES
-(3, '5', '2', 'Mark Anthony Monaya', 'cute ah', 'upload/6.jpg', ''),
-(4, '1', '2', 'Mark Anthony Monaya', 'cute pre ah .ikaw na gd na..', 'upload/6.jpg', ''),
-(5, '2', '2', 'Mark Anthony Monaya', 'bkud tnie qng nka upod ka pre..', 'upload/6.jpg', ''),
-(6, '2', '1', 'Rolyn Jasper Demerin', 'mayu pre buh .nd mn b puedi pre .ok lang na ah', 'upload/rolyn.jpg', ''),
-(7, '2', '1', 'Rolyn Jasper Demerin', 'hehehe. :d', 'upload/rolyn.jpg', ''),
-(8, '1', '1', 'Rolyn Jasper Demerin', 'wahaha . ayus pre ah', 'upload/rolyn.jpg', ''),
-(11, '3', '2', 'Mark Anthony Monaya', 'pra mai ma comment mn sa pp mu..', 'upload/6.jpg', ''),
-(12, '3', '2', 'Mark Anthony Monaya', 'pra mai ma comment mn sa pp mu..', 'upload/6.jpg', ''),
-(13, '7', '2', 'Mark Anthony Monaya', 'wahaha', 'upload/6.jpg', ''),
-(14, '7', '2', 'Mark Anthony Monaya', 'dkfjfj', 'upload/6.jpg', ''),
-(15, '7', '2', 'Mark Anthony Monaya', 'ok na?', 'upload/6.jpg', '1413322175'),
-(16, '8', '2', 'Mark Anthony Monaya', 'ok mn pre?', 'upload/6.jpg', '1413322623'),
-(18, '9', '2', 'Mark Anthony Monaya', 'kk', 'upload/6.jpg', '1413323909'),
-(19, '9', '2', 'Mark Anthony Monaya', 'kjbhkj', 'upload/6.jpg', '1413323915'),
-(21, '11', '5', 'janobe sourcecode', 'sad', 'upload/img_6993_gallery__5cac08eb3f426.jpg', '1605570528'),
-(22, '11', '5', 'janobe sourcecode', 'asd', 'upload/img_6993_gallery__5cac08eb3f426.jpg', '1605570531'),
-(23, '10', '5', 'janobe sourcecode', 'sad', 'upload/img_6993_gallery__5cac08eb3f426.jpg', '1605575090'),
-(24, '9', '1', 'Rolyn Jasper Demerin', 'What?', 'upload/rolyn.jpg', '1615823703');
+INSERT INTO `comments` (`commentid`, `uid`, `pid`, `description`) VALUES
+(1, 8, 14, 'Hey'),
+(2, 8, 14, 'This is sarath'),
+(3, 12, 14, 'heyy this is meghana'),
+(4, 8, 14, 'woww'),
+(5, 8, 14, 'Checking'),
+(6, 8, 14, 'checking'),
+(7, 8, 13, 'Haha'),
+(8, 8, 13, 'What?'),
+(9, 8, 13, ''),
+(10, 8, 12, 'Hey hey'),
+(11, 12, 12, 'Hii chintu annayya'),
+(12, 8, 14, 'Haha'),
+(13, 12, 14, 'Don\'t laugh'),
+(14, 8, 14, 'OK OK'),
+(15, 12, 14, 'Yup'),
+(16, 8, 15, 'Haha'),
+(17, 8, 12, 'haha'),
+(18, 8, 27, 'Hii everyone'),
+(19, 20, 27, 'Hii');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `commentsfound`
+--
+
+CREATE TABLE `commentsfound` (
+  `commentid` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `pid` int(11) NOT NULL,
+  `description` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `commentsfound`
+--
+
+INSERT INTO `commentsfound` (`commentid`, `uid`, `pid`, `description`) VALUES
+(0, 12, 1, 'Testing'),
+(0, 8, 6, 'Its mine');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `commentslost`
+--
+
+CREATE TABLE `commentslost` (
+  `commentid` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `pid` int(11) NOT NULL,
+  `description` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `commentslost`
+--
+
+INSERT INTO `commentslost` (`commentid`, `uid`, `pid`, `description`) VALUES
+(0, 8, 7, 'I havent found it yet!!'),
+(0, 8, 7, 'Anyone respond'),
+(0, 12, 10, 'I found it!!'),
+(0, 11, 11, 'I saw a bag near steps few minutes back!!');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `found`
+--
+
+CREATE TABLE `found` (
+  `id` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `image` varchar(200) NOT NULL,
+  `created_at` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `found`
+--
+
+INSERT INTO `found` (`id`, `uid`, `name`, `description`, `image`, `created_at`) VALUES
+(3, 15, 'bag', 'I found this bag. Contact me', 'upload/2021_05_26 09_41_32am 15 download.jfif', '1622014892'),
+(4, 15, 'water bottle', 'If this is anyone\'s bottle..Contact me.', 'upload/2021_05_26 09_42_18am 15 download (1).jfif', '1622014938'),
+(6, 8, 'anaonyous', 'I found something', 'Hello', '');
 
 -- --------------------------------------------------------
 
@@ -79,8 +145,35 @@ CREATE TABLE `friends` (
 --
 
 INSERT INTO `friends` (`id`, `user1`, `user2`) VALUES
-(2, 10, 8),
-(3, 8, 11);
+(1, 8, 11),
+(2, 11, 10),
+(3, 11, 14),
+(4, 14, 15),
+(5, 11, 15),
+(6, 11, 12),
+(7, 14, 12),
+(8, 15, 12),
+(9, 11, 13),
+(10, 12, 13),
+(11, 14, 13),
+(12, 15, 13),
+(13, 12, 8),
+(14, 14, 8),
+(16, 15, 8),
+(17, 16, 15),
+(18, 16, 8),
+(19, 18, 8),
+(20, 12, 10),
+(21, 14, 10),
+(22, 15, 10),
+(23, 16, 10),
+(24, 10, 8),
+(26, 16, 12),
+(28, 20, 10),
+(29, 20, 12),
+(30, 20, 15),
+(31, 20, 11),
+(32, 20, 8);
 
 -- --------------------------------------------------------
 
@@ -99,38 +192,131 @@ CREATE TABLE `friend_requests` (
 --
 
 INSERT INTO `friend_requests` (`id`, `sender`, `receiver`) VALUES
-(3, 8, 1),
-(5, 8, 4),
-(7, 8, 3);
+(14, 11, 5),
+(34, 16, 11),
+(37, 16, 14),
+(38, 16, 13),
+(40, 17, 10),
+(41, 17, 11),
+(42, 17, 8),
+(43, 17, 12),
+(44, 17, 15),
+(45, 17, 14),
+(46, 17, 13),
+(47, 17, 16),
+(49, 18, 11),
+(50, 18, 10),
+(51, 18, 12),
+(52, 18, 14),
+(57, 8, 13),
+(61, 20, 19);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `photos`
+-- Table structure for table `likefounds`
 --
 
-CREATE TABLE `photos` (
-  `photo_id` int(100) NOT NULL,
-  `location` varchar(100) NOT NULL,
-  `user_id` varchar(100) NOT NULL,
-  `date_added` varchar(100) NOT NULL
+CREATE TABLE `likefounds` (
+  `lid` int(11) NOT NULL,
+  `pid` int(11) NOT NULL,
+  `uid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `photos`
+-- Dumping data for table `likefounds`
 --
 
-INSERT INTO `photos` (`photo_id`, `location`, `user_id`, `date_added`) VALUES
-(1, 'upload/10355746_10201322838071324_4012919269830340563_n.jpg', '1', '2014-10-13 01:11:07'),
-(2, 'upload/1554634_934733823220509_3613827536046659520_n.jpg', '3', '2014-10-13 01:12:00'),
-(3, 'upload/10009346_637081149680216_1873786828_n.jpg', '3', '2014-10-13 01:22:41'),
-(4, 'upload/10409409_812993662052447_8357350814467004075_n.jpg', '3', '2014-10-13 01:28:18'),
-(5, 'upload/1391735_10201428940032137_674307711_n.jpg', '3', '2014-10-13 01:28:23'),
-(6, 'upload/988842_777445008951996_1989282849_n.jpg', '3', '2014-10-13 01:51:59'),
-(7, 'upload/2.jpg', '1', '2014-10-13 06:00:08'),
-(8, 'upload/10.jpg', '2', '2014-10-14 07:34:19'),
-(9, 'upload/covernirolyn.jpg', '1', '2014-10-14 18:51:36'),
-(10, 'upload/covernimark.jpg', '2', '2014-10-14 18:53:51');
+INSERT INTO `likefounds` (`lid`, `pid`, `uid`) VALUES
+(0, 6, 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likelosts`
+--
+
+CREATE TABLE `likelosts` (
+  `lid` int(11) NOT NULL,
+  `pid` int(11) NOT NULL,
+  `uid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `likelosts`
+--
+
+INSERT INTO `likelosts` (`lid`, `pid`, `uid`) VALUES
+(0, 9, 11),
+(0, 7, 11),
+(0, 10, 11),
+(0, 10, 12),
+(0, 9, 12),
+(0, 7, 12),
+(0, 11, 12),
+(0, 11, 11),
+(0, 11, 8),
+(0, 10, 8),
+(0, 9, 8),
+(0, 7, 8),
+(0, 9, 15),
+(0, 15, 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likeposts`
+--
+
+CREATE TABLE `likeposts` (
+  `lid` int(11) NOT NULL,
+  `pid` int(11) NOT NULL,
+  `uid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `likeposts`
+--
+
+INSERT INTO `likeposts` (`lid`, `pid`, `uid`) VALUES
+(130, 13, 12),
+(132, 16, 12),
+(140, 14, 8),
+(146, 12, 8),
+(149, 17, 8),
+(151, 18, 8),
+(153, 19, 8),
+(154, 13, 8),
+(155, 15, 8),
+(156, 20, 20),
+(159, 27, 20);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lost`
+--
+
+CREATE TABLE `lost` (
+  `id` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `image` varchar(600) NOT NULL,
+  `created_at` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lost`
+--
+
+INSERT INTO `lost` (`id`, `uid`, `name`, `description`, `image`, `created_at`) VALUES
+(7, 8, 'id card', 'I lost my id card.. If anyone finds it inform me', 'Hello', ''),
+(10, 11, 'watch', 'I lost my watch in the ground,If anyone finds please inform me.', 'upload/2021_05_25 17_25_06pm 11 thumb.jfif', '1621956306'),
+(11, 12, 'bag', 'I lost my bag. I forgot it near library.', 'Hello', ''),
+(14, 15, 'ring', 'I lost this ring', 'upload/2021_05_26 09_40_25am 15 images.jfif', '1622014825'),
+(15, 8, 'gold chain', 'I lost this gold Chain', 'upload/2021_05_26 12_11_13pm 8 Darcey-Gold-Chain.jpg', '1622023873'),
+(16, 8, 'bag', 'I lost my bag near ground.If anyone finds it please inform', 'upload/2021_05_26 14_20_54pm 8 download.jfif', '1622031654');
 
 -- --------------------------------------------------------
 
@@ -151,13 +337,40 @@ CREATE TABLE `post` (
 --
 
 INSERT INTO `post` (`post_id`, `user_id`, `post_image`, `content`, `created`) VALUES
-(1, '1', 'upload/postnirolyn.jpg', 'cute mark ai?', ''),
-(2, '2', 'upload/boracaycmark.jpg', 'sa boracay kmi n pre...wahahhaa.. @Rolyn', ''),
-(3, '1', 'upload/rolyn.jpg', '...ayus ai? pp qn bla', ''),
-(9, '2', 'upload/8.jpg', 'ok mn?', '1413322666'),
-(10, '2', 'upload/7.jpg', 'ok mn n?', '1413322682'),
-(11, '5', 'upload/2020-11-16 (2).png', 'sadasd', '1605570522'),
-(12, '8', 'upload/chintu.jpg', 'She proposed me today!!!!', '1616654338');
+(12, '8', 'upload/chintu.jpg', 'She proposed me today!!!!', '1616654338'),
+(13, '11', 'upload/santhan.PNG', 'Hi , I am Santhan and a topper', '1620643103'),
+(14, '15', 'upload/var.PNG', 'I am god and Devil', '1620976744'),
+(15, '12', 'upload/387645.jpg', 'I love nature', '1620977119'),
+(16, '13', 'upload/OkeluY.jpg', 'Its raining!!!', '1620977312'),
+(17, '8', 'Hello', 'Hii, I am sarath', '1621969366'),
+(18, '8', 'upload/2021_05_25 21_05_02pm 8 kishore.PNG', 'Just for fun', '1621969502'),
+(19, '8', 'upload/2021_05_26 06_25_10am 8 WIN_20210526_09_48_30_Pro.jpg', 'Heyyy, My new pic!!', '1622003110'),
+(20, '20', 'Hello', 'Hi friends, I just joined this mediaa...', '1622013767'),
+(21, '11', 'upload/2021_05_26 09_46_57am 11 OkeluY.jpg', 'Heyy, Just wanted to share that I got this..', '1622015217'),
+(22, '8', 'upload/2021_05_26 10_24_07am 8 krish.PNG', 'with MOM', '1622017447'),
+(24, '8', 'upload/2021_05_26 10_27_15am 8 64405508_467673843995285_3067242068280606720_n.jpg', 'Heyyyy', '1622017635'),
+(25, '20', 'upload/2021_05_26 12_38_36pm 20 sarath.jpg', 'good evening', '1622025516'),
+(26, '8', 'Hello', 'Good evening evryone', '1622031348');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `saveposts`
+--
+
+CREATE TABLE `saveposts` (
+  `id` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `pid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `saveposts`
+--
+
+INSERT INTO `saveposts` (`id`, `uid`, `pid`) VALUES
+(4, 8, 13),
+(6, 8, 21);
 
 -- --------------------------------------------------------
 
@@ -185,14 +398,17 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `username`, `birthday`, `gender`, `number`, `branch`, `email`, `password`, `profile_picture`, `cover_picture`) VALUES
-(1, 'Rolyn Jasper', 'Demerin', 'revengeHatred', '13/November/1995', 'male', '09989781348', 'CSE', 'rolyn02@gmail.com', '12345', 'upload/rolyn.jpg', 'upload/covernirolyn.jpg'),
-(2, 'Mark Anthony', 'Monaya', 'bobaytot11', '1995-11-13', 'Male', '09989781346', 'CSE', 'markmonaya@gmail.com', '123456', 'upload/6.jpg', 'upload/covernimark.jpg'),
-(3, 'Jhonalyn', 'Montero', 'jho_phet', '14/June/1996', 'female', '09285444196', 'CSE', 'jho_montero@gmail.com', 'jhopeta', 'upload/400076_2586928959209_1713686254_n.jpg', ''),
-(4, 'Shaira', 'Gaston', 'djBatman', '1/January/1901', 'female', '09989781356', 'CSE', 'shaira_gaston@gmail.com', '1234567', 'upload/1554634_934733823220509_3613827536046659520_n.jpg', ''),
-(5, 'janobe', 'sourcecode', 'admin', '1/January/1901', 'male', '09305235022', 'CSE', 'janobe@gmail.com', 'admin', 'upload/img_6993_gallery__5cac08eb3f426.jpg', 'upload/nacpan-beach-el-nido-4.jpg'),
-(8, 'S C Sai SatyaNarayana', 'Gudimetla', 'sarath143', '1/January/1901', 'male', '8790360069', 'CSE', 'sarathchandhra365@gmail.com', '$2y$10$urt6sp2a..cB97GV0uvhSuvYnNryQBMG4.4oyAI99ndDOQAO4f5ve', 'upload/sarath.jpg', ''),
+(8, 'SatyaNarayana', 'Gudimetla', 'sarath143', '23/10/1999', 'Male', '8790360069', 'CSE', 'sarathchandhra365@gmail.com', '$2y$10$ft04szMy1dNSiX4TbjQt3uNNnePembCpZ2Bry5TPauiTMJZklUjHa', 'upload/2021_05_20 10_12_41am 8 IMG_20210512_154856.jpg', 'upload/  8 Screenshot (113).png'),
 (10, 'banti', 'karri', 'banti', '23/10/1999', 'Male', '7903600691', 'MECH', 'banti@gmail.com', '$2y$10$O8kwJ61lSX6GRP1XxdjZRe776GavPJIaZAkJg3sF.qE1LwaDqDOyS', 'upload/OkeluY.jpg', ''),
-(11, 'Santhan', 'Edara', 'santhan', '24/03/1999', 'Male', '7674967510', 'ECE', 'edarasanthan.1999@gmail.com', '$2y$10$W2UPyzjRhFcD6tNCjJ.pduUp//M.va6QBJU5XOzrw/5UIjFJB4B7e', 'upload/santhan.PNG', '');
+(11, 'Santhan', 'Edara', 'santhan', '24/03/1999', 'Male', '7674967510', 'ECE', 'edarasanthan.1999@gmail.com', '$2y$10$W2UPyzjRhFcD6tNCjJ.pduUp//M.va6QBJU5XOzrw/5UIjFJB4B7e', 'upload/santhan.PNG', 'upload/  11 andromeda-galaxy-wallpaper-hd-02.jpg'),
+(12, 'Meghana', 'Karri', 'meghana', '21/10/2002', 'Female', '9585647563', 'CSE', 'meghana@gmail.com', '$2y$10$HaFCQx/SKPWcb2ErbpgmduwccEWTTRsSzaYo4fTYy7/tYw2htuW7S', 'upload/2021_05_14 09_24_49am 12 megha.PNG', 'upload/  12 387645.jpg'),
+(13, 'krishnaveni', 'gudimetla', 'krishve', '14/06/1880', 'Female', '9704503658', 'CIVIL', 'gkvreddy3658@gmail.com', '$2y$10$ab.ZsfnO1ANuLscrrqAD2uGCYbBpLoToy/5mXuLvpM.pjUBIY9h2q', 'upload/2021_05_14 09_27_42am 13 krish.PNG', 'upload/  13 OkeluY.jpg'),
+(14, 'Keerthana', 'Achalla', 'keerthi', '08/10/1999', 'Female', '8790857001', 'CSE', 'keerthicherry999@gmail.com', '$2y$10$7t7K7xRChB9f2O/w4f0AdumGfzDGjdjgbaHufM3YatnkkoK5s57gi', 'upload/2021_05_14 08_44_27am 14 keer.PNG', 'upload/  14   8 Screenshot (113).png'),
+(15, 'Varshith', 'Gummadi', 'varsit', '04/06/1999', 'Male', '4561237895', 'MECH', 'varshithgummadi10@gmail.com', '$2y$10$eg.xZ4HFFxiB1nemwtZFBehdDpmBmGULIx2KIwBQUrosr.lWaJHkS', 'upload/2021_05_14 09_18_29am 15 var.PNG', 'upload/  15 HD-Galaxy-Wallpapers-Free-Download-115.jpg'),
+(17, 'ganesh', 'perumalla', 'gannu', '04/06/1999', 'Male', '8795641235', 'MECH', 'pthushara2001@gmail.com', '$2y$10$EDgzP1yAcNWOSbIpwqokQOY4DutPmLW7RZAQpQhtgCanJfzX0AL1a', 'upload/2021_05_21 19_16_54pm 17 ganesh.PNG', 'upload/  17 HD-Galaxy-Wallpapers-Free-Download-115.jpg'),
+(18, 'balakishore', 'kongara', 'nenuchesta', '21/01/2000', 'Male', '9542008445', 'CSE', 'kongabalakishore@gmail.com', '$2y$10$zNZzr6s8ZmxfWAhc8z1EX.4rAFBSgJDbExEQj21J/Us29mewEClw2', 'upload/2021_05_21 19_36_44pm 18 kishore.PNG', 'upload/  18 OkeluY.jpg'),
+(19, 'thanu', 'thamanampudi', 'thanu', '04/09/2004', 'Female', '1234567890', 'CIVIL', 'thanu@gmail.com', '$2y$10$/g3U4QBC6v0ojSz6.mStR.q5i3wl/iAvdqiQy12OX..Vs9KdgDHfO', 'upload/2021_05_23 17_31_52pm 19 bg12.jpg', 'upload/  19 andromeda-galaxy-wallpaper-hd-02.jpg'),
+(20, 'Sai ', 'Reddy', 'sai', '24/10/1999', 'Male', '9542008445', 'CSE', 'scssreddy.gudimetla@vitap.ac.in', '$2y$10$Y.kxtCw60eG0v6n/sDe8neHaGff4fYshYu8Qm9mQ5MsMDzF8klJpO', 'upload/2021_05_26 09_22_23am 20 sarath.jpg', 'upload/2021_05_26 09_22_13am 20 pexels-photo-949587.jpeg');
 
 --
 -- Indexes for dumped tables
@@ -202,7 +418,13 @@ INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `username`, `birthday`, 
 -- Indexes for table `comments`
 --
 ALTER TABLE `comments`
-  ADD PRIMARY KEY (`comment_id`);
+  ADD PRIMARY KEY (`commentid`);
+
+--
+-- Indexes for table `found`
+--
+ALTER TABLE `found`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `friends`
@@ -217,16 +439,28 @@ ALTER TABLE `friend_requests`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `photos`
+-- Indexes for table `likeposts`
 --
-ALTER TABLE `photos`
-  ADD PRIMARY KEY (`photo_id`);
+ALTER TABLE `likeposts`
+  ADD PRIMARY KEY (`lid`);
+
+--
+-- Indexes for table `lost`
+--
+ALTER TABLE `lost`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `post`
 --
 ALTER TABLE `post`
   ADD PRIMARY KEY (`post_id`);
+
+--
+-- Indexes for table `saveposts`
+--
+ALTER TABLE `saveposts`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
@@ -242,37 +476,55 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `comment_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `commentid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `found`
+--
+ALTER TABLE `found`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `friends`
 --
 ALTER TABLE `friends`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT for table `friend_requests`
 --
 ALTER TABLE `friend_requests`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 
 --
--- AUTO_INCREMENT for table `photos`
+-- AUTO_INCREMENT for table `likeposts`
 --
-ALTER TABLE `photos`
-  MODIFY `photo_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+ALTER TABLE `likeposts`
+  MODIFY `lid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=160;
+
+--
+-- AUTO_INCREMENT for table `lost`
+--
+ALTER TABLE `lost`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
-  MODIFY `post_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `post_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `saveposts`
+--
+ALTER TABLE `saveposts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `user_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
